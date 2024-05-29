fc79a9031f2454bb366d87381a9c4a10e4267cd0
SoundDrill31 <84176052+sounddrill31@users.noreply.github.com>
2024-05-29 17:57:24 +0530

    Generate_Keys.sh: Initial adaptation
    Take Folder path as argument

--- signing/Generate_Keys.sh
+++ signing/Generate_Keys.sh
@@ -1,22 +1,29 @@
 #!/bin/bash
 
+# Credit to ObsidianMaximus for the script
+# To be used with https://github.com/sounddrill31/crave_aosp_builder/blob/signing/scripts/signing-script.sh
+# run this: 
+# curl https://raw.githubusercontent.com/sounddrill31/crave_aosp_builder/signing/scripts/signing-script.sh; KEYS_DIR=$(realpath "../private/$(basename "$PWD")"); bash signing-script.sh $KEYS_DIR
+
+dir_path=$1
+
 # Step 1
 
 subject='/C=US/ST=California/L=Mountain View/O=Android/OU=Android/CN=Android/emailAddress=android@android.com'
-mkdir ~/.android-certs
+mkdir $dir_path
 for cert in bluetooth cyngn-app media networkstack platform releasekey sdk_sandbox shared testcert testkey verity; do \
-    ./development/tools/make_key ~/.android-certs/$cert "$subject"; \
+    ./development/tools/make_key $dir_path/$cert "$subject"; \
 done
 
 # Step 2
 
-cp ./development/tools/make_key ~/.android-certs/
-sed -i 's|2048|4096|g' ~/.android-certs/make_key
+cp ./development/tools/make_key $dir_path/
+sed -i 's|2048|4096|g' $dir_path/make_key
 
 # Step 3
 
 for apex in com.android.adbd com.android.adservices com.android.adservices.api com.android.appsearch com.android.art com.android.bluetooth com.android.btservices com.android.cellbroadcast com.android.compos com.android.configinfrastructure com.android.connectivity.resources com.android.conscrypt com.android.devicelock com.android.extservices com.android.graphics.pdf com.android.hardware.biometrics.face.virtual com.android.hardware.biometrics.fingerprint.virtual com.android.hardware.boot com.android.hardware.cas com.android.hardware.wifi com.android.healthfitness com.android.hotspot2.osulogin com.android.i18n com.android.ipsec com.android.media com.android.media.swcodec com.android.mediaprovider com.android.nearby.halfsheet com.android.networkstack.tethering com.android.neuralnetworks com.android.ondevicepersonalization com.android.os.statsd com.android.permission com.android.resolv com.android.rkpd com.android.runtime com.android.safetycenter.resources com.android.scheduling com.android.sdkext com.android.support.apexer com.android.telephony com.android.telephonymodules com.android.tethering com.android.tzdata com.android.uwb com.android.uwb.resources com.android.virt com.android.vndk.current com.android.vndk.current.on_vendor com.android.wifi com.android.wifi.dialog com.android.wifi.resources com.google.pixel.camera.hal com.google.pixel.vibrator.hal com.qorvo.uwb; do \
     subject='/C=US/ST=California/L=Mountain View/O=Android/OU=Android/CN='$apex'/emailAddress=android@android.com'; \
-    ~/.android-certs/make_key ~/.android-certs/$apex "$subject"; \
-    openssl pkcs8 -in ~/.android-certs/$apex.pk8 -inform DER -out ~/.android-certs/$apex.pem; \
+    $dir_path/make_key $dir_path/$apex "$subject"; \
+    openssl pkcs8 -in $dir_path/$apex.pk8 -inform DER -out $dir_path/$apex.pem; \
 done
